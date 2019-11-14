caec7d880059906d00e10115f37e445eac73ed3b
Rosemary Binoy <43945196+NjanRose@users.noreply.github.com>
2019-11-15 00:59:59 +0530

    ConstableUI

--- app/src/main/java/com/project/coders/ksp/Login.java
+++ app/src/main/java/com/project/coders/ksp/Login.java
@@ -2,13 +2,23 @@ package com.project.coders.ksp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button loginbutton = findViewById(R.id.button);
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //for now this is going to constable page
+                Intent intent = new Intent(Login.this,Constable.class);
+                startActivity(intent);
+            }
+        });
     }
 }
