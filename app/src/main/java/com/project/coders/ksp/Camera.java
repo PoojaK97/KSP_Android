a7295deed7441a5747079a29145663629cd11945
PoojaK97 <thisispooja97@gmail.com>
2019-11-15 08:58:44 +0530

    Camera ...

---
+++ app/src/main/java/com/project/coders/ksp/Camera.java
@@ -0,0 +1,111 @@
+package com.project.coders.ksp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Camera extends AppCompatActivity {
+    private Button btnCapture;
+    private Button btnCapture1;
+    private ImageView imgCapture;
+    private String filename;
+    //private Uri imageFileUri;
+    private StorageReference mStorageRef;
+    private static final int Image_Capture_Code = 1;
+    private static final int VIDEO_CAPTURE = 101;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+        btnCapture = findViewById(R.id.btnTakePicture);
+        btnCapture1 = findViewById(R.id.button1);
+        imgCapture = findViewById(R.id.capturedImage);
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+        btnCapture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent cInt = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cInt,Image_Capture_Code);
+
+            }
+        });
+        btnCapture1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                startActivityForResult(intent, VIDEO_CAPTURE);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == Image_Capture_Code) {
+            if (resultCode == RESULT_OK) {
+
+
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            }
+        }
+        if (requestCode == VIDEO_CAPTURE) {
+            Uri videoUri = data.getData();
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Video saved to:\n" +
+                        videoUri, Toast.LENGTH_LONG).show();
+
+                DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+                Date date = new Date();
+                filename = "VID_" + dateFormat.format(date).replaceAll("\\s+", "").replaceAll("/", "_");
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                String uid = user.getUid();
+                StorageReference riversRef = mStorageRef.child("Beats/" + uid + "/" + filename);
+
+                riversRef.putFile(videoUri)
+                        .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                            @Override
+                            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                // Get a URL to the uploaded content
+                                //Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                                System.out.println("sucess upload");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception exception) {
+                                // Handle unsuccessful uploads
+                                // ...
+                            }
+                        });
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "Video recording cancelled.",
+                        Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Failed to record video",
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+}
--- app/src/main/java/com/project/coders/ksp/Constable.java
+++ app/src/main/java/com/project/coders/ksp/Constable.java
@@ -2,7 +2,11 @@ package com.project.coders.ksp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class Constable extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class Constable extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_constable);
+
+        ImageView cam = findViewById(R.id.imageButton);
+
+        cam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Constable.this,Camera.class));
+                finish();
+            }
+        });
     }
 }
