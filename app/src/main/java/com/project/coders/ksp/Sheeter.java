c47df776d25492c47dc9571eb169a9485b0ca64c
Njanrose <rosemarybinoym@gmail.com>
2019-11-17 00:20:28 +0530

    ..

---
+++ app/src/main/java/com/project/coders/ksp/Sheeter.java
@@ -0,0 +1,14 @@
+package com.project.coders.ksp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Sheeter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sheeter);
+    }
+}
